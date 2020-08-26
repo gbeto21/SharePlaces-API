4b928911143934e8c79b6742d0d7d1482d694235
Alberto González Balderas <albertoglezb@hotmail.com>
2020-08-26 07:06:52 -0500

    Validations to the post place request were added.

--- controllers/places-controllers.js
+++ controllers/places-controllers.js
@@ -1,4 +1,7 @@
 const { v4: uuidv4 } = require('uuid');
+
+const { validationResult } = require('express-validator')
+
 const HttpError = require('../models/http-error')
 
 let DUMMY_PLACES = [
@@ -42,6 +45,12 @@ const getPlacesByUserId = (req, res, next) => {
 }
 
 const createPlace = (req, res, next) => {
+
+    const errors = validationResult(req)
+    if (!errors.isEmpty()) {
+        throw new HttpError('Invalid input passed, please check your data.', 422)
+    }
+
     const { title, description, coordinates, address, creator } = req.body
     const createdPlace = {
         id: uuidv4(),
