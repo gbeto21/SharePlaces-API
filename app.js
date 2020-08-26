a2827c5ce6b71436238aeb254615362233ac0a5c
Alberto González Balderas <albertoglezb@hotmail.com>
2020-08-26 11:55:08 -0500

    The connection to the database was implemented.

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const express = require('express')
 const bodyParser = require('body-parser')
+const mongoose = require('mongoose')
 
 const placesRoutes = require('./routes/places-routes')
 const usersRoutes = require('./routes/user-routes')
 const HttpError = require('./models/http-error')
 const app = express()
+const config = require('./config')
 
 app.use(bodyParser.json())
 app.use('/api/places', placesRoutes)
@@ -22,4 +24,11 @@ app.use((error, req, res, next) => {
     res.json({ messaje: error.message || 'An unknow error occurred!' })
 })
 
-app.listen(5000)
+mongoose
+    .connect(`mongodb+srv://${config.db.user}:${config.db.password}@${config.db.cluster}.xuzor.mongodb.net/${config.db.database}?retryWrites=true&w=majority`)
+    .then(() => {
+        app.listen(5000)
+    })
+    .catch(err => {
+        console.log(err);
+    })
