2fe9cc9b534ab58881b864784afd700f0d454370
Alberto González Balderas <albertoglezb@hotmail.com>
2020-08-26 17:44:23 -0500

    The funtionality of save to Mongo using the schema was created.

--- controllers/places-controllers.js
+++ controllers/places-controllers.js
@@ -3,6 +3,7 @@ const { v4: uuidv4 } = require('uuid');
 const { validationResult } = require('express-validator')
 
 const HttpError = require('../models/http-error')
+const Place = require('../models/place')
 
 let DUMMY_PLACES = [
     {
@@ -44,7 +45,7 @@ const getPlacesByUserId = (req, res, next) => {
     res.json(places)
 }
 
-const createPlace = (req, res, next) => {
+const createPlace = async (req, res, next) => {
 
     const errors = validationResult(req)
     if (!errors.isEmpty()) {
@@ -52,16 +53,23 @@ const createPlace = (req, res, next) => {
     }
 
     const { title, description, coordinates, address, creator } = req.body
-    const createdPlace = {
-        id: uuidv4(),
+    const createdPlace = new Place({
         title,
         description,
-        location: coordinates,
         address,
+        location: coordinates,
+        image: 'https://upload.wikimedia.org/wikipedia/commons/1/10/Empire_State_Building_%28aerial_view%29.jpg',
         creator
-    }
+    })
 
-    DUMMY_PLACES.push(createdPlace)
+    try {
+
+        await createdPlace.save()
+
+    } catch (error) {
+        const err = new HttpError('Creating place failed, please try again.', 500)
+        return next(err)
+    }
 
     res.status(201).json({ place: createdPlace })
 }
