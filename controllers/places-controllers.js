f16fa4310b46f7e06f08599914b1b940a1c199e4
Alberto González Balderas <albertoglezb@hotmail.com>
2020-08-27 07:54:32 -0500

    The funtionality of delete a place from the database and the relation from the user was created.

--- controllers/places-controllers.js
+++ controllers/places-controllers.js
@@ -165,20 +165,31 @@ const deletePlace = async (req, res, next) => {
 
     let place
     try {
-        place = await Place.findById(placeId)
+        place = await Place.findById(placeId).populate('creator')
     } catch (error) {
         const err = new HttpError(
-            'Something went wrong, could not delete place',
+            'Something went wrong finding the place, could not delete place',
             500
         )
         return next(err)
     }
 
+    if (!place) {
+        const error = new HttpError('Could not find place for this id.', 404)
+        return next(error)
+    }
+
     try {
-        await place.remove()
+        const sess = await mongoose.startSession()
+        sess.startTransaction()
+        await place.remove({ session: sess })
+        place.creator.places.pull(place)
+        await place.creator.save({ session: sess })
+        await sess.commitTransaction()
     } catch (error) {
+        console.log(error);
         const err = new HttpError(
-            'Something went wrong, could not delete place',
+            'Something went wrong deleting the place, could not delete place',
             500
         )
         return next(err)
