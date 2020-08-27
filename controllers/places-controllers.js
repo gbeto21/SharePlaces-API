4b84995b67b986e7b07b527f4ac04156b4d16225
Alberto González Balderas <albertoglezb@hotmail.com>
2020-08-27 08:04:44 -0500

    The funtionality of get the places by user id was changed to another way.

--- controllers/places-controllers.js
+++ controllers/places-controllers.js
@@ -45,11 +45,12 @@ const getPlaceById = async (req, res, next) => {
 
 const getPlacesByUserId = async (req, res, next) => {
     const userId = req.params.uid
-    let places
+    //let places
+    let userWithPlaces
 
     try {
 
-        places = await Place.find({ creator: userId })
+        userWithPlaces = await User.findById(userId).populate('places')
 
     } catch (error) {
         const err = new HttpError(
@@ -58,12 +59,13 @@ const getPlacesByUserId = async (req, res, next) => {
         return next(err)
     }
 
-    if (!places || places.length === 0) {
+    if (!userWithPlaces || userWithPlaces.places.length === 0) {
         return next(new HttpError('Could not find places for the provided user id.', 404))
     }
 
     res.json({
-        places: places
+        places: userWithPlaces
+            .places
             .map(
                 place => place.toObject({ getters: true })
             )
