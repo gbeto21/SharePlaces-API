9e45cdb42d220b04d332a0e5b751432b727a13b1
Alberto González Balderas <albertoglezb@hotmail.com>
2020-08-29 12:57:56 -0500

    We allow to add new places to the users.

--- middleware/check-auth.js
+++ middleware/check-auth.js
@@ -4,6 +4,10 @@ const HttpError = require("../models/http-error")
 const config = require('../config')
 module.exports = (req, res, next) => {
 
+    if (req.method === 'OPTIONS') {
+        return next()
+    }
+
     try {
         const token = req.headers.authorization.split(' ')[1]
         if (!token) {
