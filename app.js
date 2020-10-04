4b1763ae55f75dbfcec7c2a9ad5130e70d3ba9cf
Alberto González Balderas <albertoglezb@hotmail.com>
2020-10-04 18:02:31 -0500

    Debuggin in production.

--- app.js
+++ app.js
@@ -37,7 +37,10 @@ app.use((error, req, res, next) => {
     if (res.headerSent) {
         return next(error)
     }
-    res.status(error.code || 500)
+    res.status(
+        // error.code 
+        // || 
+        500)
     res.json({ message: error.message || 'An unknow error occurred!' })
 })
 
